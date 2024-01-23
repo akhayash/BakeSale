7208273a973e47a703802e774a57bcad3622c610
akhayash <akhayash@microsoft.com>
2024-01-23 12:28:28 +0900

    Working with images

---
+++ components/DealItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ProtoTypes from 'prop-types';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {priceDisplay} from '../util';
+
+class DealItem extends React.Component {
+  static propTypes = {
+    deal: ProtoTypes.object.isRequired,
+  };
+  render() {
+    const {deal} = this.props;
+    return (
+      <View>
+        <Image source={{uri: deal.media[0]}} style={styles.image} />
+        <View>
+          <Text>{deal.title}</Text>
+          <Text>{priceDisplay(deal.price)}</Text>
+          <Text>{deal.cause.name}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 150,
+  },
+});
+
+export default DealItem;
